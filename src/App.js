be31ff59cba20c70d9e43a0cbedc3bc5699dcffd
Allison Budziak Wu <112855952+adbwu@users.noreply.github.com>
2023-02-28 21:48:31 -0500

    rect testing

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <svg viewBox="0 0 100 100">
+          <rect x="44.5" y="44.5" height="11" width="11" style={{ fill: '#fff' }}></rect>
+        </svg>
       </header>
     </div>
   );
